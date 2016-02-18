296b8517063469632939202858bdf13d3e4855bd
Matias Quinteros <matias.quinteros@usach.cl>
2016-02-17 23:37:25 -0300

    Establecida y configurada conexion con mysql y insertados usuarios y password del servidor

--- ServidorJavaRMI/src/servidorRMI/Main.java
+++ ServidorJavaRMI/src/servidorRMI/Main.java
@@ -1,3 +1,9 @@
+package servidorRMI;
+
+import swing.User;
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +16,6 @@
  */
 public class Main {
      public static void main(String[] args) {
-     
+         new User().setVisible(true);
      }
 }
