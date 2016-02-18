2b06ed83116788f66d4fc71be5a4d14b3a4cd03e
Matias Quinteros <matias.quinteros@usach.cl>
2016-02-17 22:50:00 -0300

    Main rmi

--- ServidorJavaRMI/src/Main.java
+++ ServidorJavaRMI/src/Main.java
@@ -3,15 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package servidorjavarmi;
 
 /**
  *
  * @author Matias Quinteros
  */
 public class Main {
-    public static void main(String[] args) {
-        
-        
-    }
+     public static void main(String[] args) {
+     
+     }
 }
